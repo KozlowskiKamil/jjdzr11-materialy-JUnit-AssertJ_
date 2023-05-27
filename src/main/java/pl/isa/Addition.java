873f0e6ef8b76a5945cf4925f1435bbbf937fe21
Tomasz Odwald <tomasz.odwald@gmail.com>
2023-05-27 17:35:19 +0200

    Completed ex 2

--- src/main/java/pl/isa/Addition.java
+++ src/main/java/pl/isa/Addition.java
@@ -4,12 +4,18 @@ public class Addition {
     public static int add(int... values) {
         int result = 0;
         for (int value : values) {
+            if (value > 0 && result + value < result) {
+                throw new UnsupportedOperationException("Numbers exceed max value");
+            }
+            if (value < 0 && result + value > result) {
+                throw new UnsupportedOperationException("Numbers exceed min value");
+            }
             result += value;
         }
         return result;
     }
 
     public static void main(String[] args) {
-        System.out.println(Addition.add(2, 2));
+        System.out.println(Addition.add());
     }
 }
