1729a3473fd99f4fc1d888704320a8640092e409
Tomasz Odwald <tomasz.odwald@gmail.com>
2023-05-28 14:15:15 +0200

    Completed ex 1

--- src/test/java/pl/isa/AdditionTests.java
+++ src/test/java/pl/isa/AdditionTests.java
@@ -1,7 +1,5 @@
 package pl.isa;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -9,6 +7,8 @@ import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.Arrays;
 import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class AdditionTests {
     private static Stream<ValuesWithSum> provideListOfMultipleIntValues() {
--- src/test/java/pl/isa/FirstTest.java
+++ src/test/java/pl/isa/FirstTest.java
@@ -1,9 +1,37 @@
 package pl.isa;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 public class FirstTest {
+
+    @BeforeAll
+    public static void setup() {
+        System.out.println("Setup");
+    }
+
     @Test
     public void firstTest() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void secondTest() {
+        assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    @Disabled
+    public void disabledTest() {
+        fail();
+    }
+
+    @AfterEach
+    public void testCleanUp() {
+        System.out.println("Test clean up");
     }
 }
