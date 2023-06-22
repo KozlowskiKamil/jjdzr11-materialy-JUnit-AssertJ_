a154ca92215a90ff922ba9ea3f8ac13c12744347
KozlowskiKamil <kamil2k8@gmail.com>
2023-06-22 14:56:08 +0200

    zad palindrom text

--- src/test/java/pl/isa/AdditionTests.java
+++ src/test/java/pl/isa/AdditionTests.java
@@ -1,8 +1,11 @@
 package pl.isa;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class AdditionTests {
 
@@ -22,6 +25,24 @@ public class AdditionTests {
     @Test
     public void add_minValueShouldThrowUnsupportedOperationException() {
         Exception exception = assertThrows(UnsupportedOperationException.class, () -> Addition.add(Integer.MIN_VALUE, -1));
-        assertEquals("Numbers exceed MIN", exception.getMessage(),"Wrong mesage");
+        assertEquals("Numbers exceed MIN", exception.getMessage(), "Wrong mesage");
     }
+
+    @Test
+    public void noValueGivern_Retuns0() {
+        assertEquals(0, Addition.add(), "Shuld 0");
+    }
+
+    @Test
+    public void singleValue() {
+        assertEquals(7, Addition.add(7), "Shuld be 7");
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {Integer.MAX_VALUE - 1, Integer.MIN_VALUE + 1, Integer.MAX_VALUE, Integer.MIN_VALUE, -4, 1, 4, 5, 7})
+    public void parametrValue(int value) {
+        assertEquals(value, Addition.add(value));
+    }
+
+
 }
